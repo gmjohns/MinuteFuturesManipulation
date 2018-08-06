8a72febf49ca5bd46c10242932bc493308a9b2d3
Garrett Johnson <gmjohns@g.clemson.edu>
2018-08-06 14:39:23 -0400

    now properly includes remainder of volume bars when calculating next point

--- compress.py
+++ compress.py
@@ -12,30 +12,29 @@ def get_vpp(data):
 def make_seq(data, vpp):
     rv = 0
     i = 0
-    new_ser = []
+    new_ser = [[0, data['volume'][0]]]
 
     while i < data['volume'].size:
         while rv < vpp and i < data['volume'].size:
             rv += data['volume'][i]
             i += 1
-        if i != data['volume'].size:
-            new_ser.append(i)
-        rv = rv-vpp
-    return pd.Series(new_ser)
+        rv = rv - vpp
+        new_ser.append([i-1, rv])
+    return pd.DataFrame(new_ser, columns=['index', 'rem'])
 
 
 def compress(data, seq):
     n = 0
     full = []
-    print(seq)
-    while n <= seq.size-2:
-        segment = data.iloc[[seq[n],seq[n+1]], :]
+    while n <= seq.size/2-2:
+        segment = data.iloc[seq['index'][n]:seq['index'][n+1]+1, :].copy()
+        segment.iloc[0, 6] = seq['rem'][n]
+        segment.iloc[-1, 6] = segment.iloc[-1, 6] - seq['rem'][n+1]
+        print(segment)
         point = [str(segment.iloc[-1, 0]), str(segment.iloc[-1, 1]),
-                np.average(segment.iloc[:, 3], weights=segment.iloc[:, 6])]
+                 np.average(segment.iloc[:, 3], weights=segment.iloc[:, 6])]
         n += 1
         full.append(point)
-        '''print(point)'''
-
     return pd.DataFrame(full, columns=['date', 'time', 'vwa'])
 
 
@@ -44,7 +43,9 @@ ppd = 2
 raw = pd.read_csv(os.getcwd() + '/minute_futures_data/' + fname + '.txt', sep=",", header=None)
 raw.columns = ['date', 'time', 'open', 'close', 'high', 'low', 'volume']
 vpp = get_vpp(raw)
+print(vpp)
 seq = make_seq(raw, vpp)
+print(seq)
 cdf = compress(raw, seq)
 cdf.to_csv(fname + 'compressed.csv', sep=',', index=False)
 
